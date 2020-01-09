e31adec7c76027d841aeb5d422f6757e5dffe064
unknown <radnyongtawco123@gmail.com>
2020-01-10 07:58:27 +0800

    first upload

---
+++ src/component/homepage/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../homepage/homepage.styles.scss';
+
+export const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> HATS </h1>
+                    <span className="subtitle"> SHOP NOW </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> MEN </h1>
+                    <span className="subtitle"> SHOP NOW </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> WOMEN </h1>
+                    <span className="subtitle"> SHOP NOW </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> SHOES </h1>
+                    <span className="subtitle"> SHOP NOW </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> ARMOR </h1>
+                    <span className="subtitle"> SHOP NOW </span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
