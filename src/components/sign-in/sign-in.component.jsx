d37ecff23c7a1b83e0169d3b6f0d08709512cc5a
unknown <radnyongtawco123@gmail.com>
2020-01-24 00:05:29 +0800

    created the function sign in handleSubmit() method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,22 +4,35 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            email:'radnyongtawco123@gmail.com',
-            password:'chickendinner'
+            email:'',
+            password:''
         }
 
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email:'', password:''});
+        const { email, password } = this.state;
+        
+        try {
+            await auth.signInWithEmailAndPassword(
+                email,
+                password
+            );
+
+            this.setState({email:'', password:''});
+
+        } catch (error) {
+            console.error(error);
+        }
+
     }
 
     handleChange = (event) => {
