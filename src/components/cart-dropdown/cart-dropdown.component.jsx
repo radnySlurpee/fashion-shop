4af6c7ccbb843e36cbd7a174f91361202d2a9055
unknown <radnyongtawco123@gmail.com>
2020-02-02 03:42:07 +0800

    updated our APP with the reselector from redux, replaced cart selectors with memoized selectors to reused data from the cache

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 const CartDropDown = ({cartItems}) =>(
    <div className='cart-dropdown'>
@@ -21,8 +22,8 @@ const CartDropDown = ({cartItems}) =>(
    </div> 
 );
 
-const mapStateToProps = ({cart: {cartItems }}) => ({
-    cartItems
-})
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+});
 
 export default connect(mapStateToProps)(CartDropDown);
